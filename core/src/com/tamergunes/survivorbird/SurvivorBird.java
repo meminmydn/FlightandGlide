ef0b7b9076f3ee3992c1727390872860ec6e9ebf
MEHMET\Mehmet Emin MEYDAN <mehmet.meydan@ogr.sakarya.edu.tr>
2019-12-22 16:14:14 +0300

    Create method added

--- core/src/com/tamergunes/survivorbird/SurvivorBird.java
+++ core/src/com/tamergunes/survivorbird/SurvivorBird.java
@@ -49,7 +49,58 @@ public class SurvivorBird extends ApplicationAdapter {
   Circle[] enemyCircle1;
   Circle[] enemyCircle2;
   Circle[] enemyCircle3;
-  
+
+  @Override
+  public void create() {
+    batch = new SpriteBatch();
+
+
+    preferences = Gdx.app.getPreferences("My preferences");
+
+    background = new Texture("background.png");
+    bird = new Texture("yarasa.png");
+    bee1 = new Texture("yarasadusman.png");
+    bee2 = new Texture("yarasadusman.png");
+    bee3 = new Texture("yarasadusman.png");
+    distance = Gdx.graphics.getWidth() / 5;//kuslar arasi mesafe genisligin yarisi olsun
+    random = new Random();
+    birdX = Gdx.graphics.getWidth() / 5;
+    birdY = Gdx.graphics.getHeight() / 3;
+
+    //shapeRenderer=new ShapeRenderer();
+
+    birdcicle = new Circle();
+    enemyCircle1 = new Circle[numberOfEnemys];
+    enemyCircle2 = new Circle[numberOfEnemys];
+    enemyCircle3 = new Circle[numberOfEnemys];
+
+    font = new BitmapFont();
+    font.setColor(Color.GREEN);
+    font.getData().setScale(4);
+
+    font2 = new BitmapFont();
+    font2.setColor(Color.GREEN);
+    font2.getData().setScale(6);
+
+
+    for (int i = 0; i < numberOfEnemys; i++) {
+
+      enemyOffSet[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+      enemyOffSet1[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+      enemyOffSet2[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+
+      enemyX[i] = Gdx.graphics.getWidth() - bee1.getWidth() / 2 + i * distance;
+      enemyCircle1[i] = new Circle();
+      enemyCircle2[i] = new Circle();
+      enemyCircle3[i] = new Circle();
+
+    }
+
+    
+  }
+
+
+
 
   @Override
   public void dispose() {
